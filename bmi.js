b5975a23b7455fb27bf22691b36585a2259db166
gopherproxy <gopherproxy@gmail.com>
2019-09-30 12:20:08 +0200

    BMI calculator default

---
+++ bmi.js
@@ -0,0 +1,15 @@
+function calculateBMI(){
+	'use strict';
+	
+}
+
+/*
+The BMI is defined as the body mass divided by the square of the body height, and is universally expressed in units of kg/m2 (source: Wikipedia)
+
+BMI = weight (kg) / (height * height (m))
+
+Underweight < 18.5
+Normal (healthy weight) 18.5 - 25
+Overweight 25 - 30
+Obesity > 30
+*/
